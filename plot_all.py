25acb580ff6935ef28ccce6bae0e117996506b5a
helgi <helgi.runarsson@gmail.com>
2015-01-27 14:07:21 +0000

    updates

--- plot_all.py
+++ plot_all.py
@@ -11,16 +11,16 @@ def plot_gogn(r,g,b):
     B_gogn_2nd = get_Bstar_data('1.0','*','2nd')
     e_gogn_1st = get_extremal_data('1.0','*','1st','*')
     e_gogn_2nd = get_extremal_data('1.0','*','2nd','*')
-    ws = gogn[:,18]
-    Ms = gogn[:,7]
-    e_ws_1st = e_gogn_1st[:,18]
-    e_Ms_1st = e_gogn_1st[:,7]
-    e_ws_2nd = e_gogn_2nd[:,18]
-    e_Ms_2nd = e_gogn_2nd[:,7]
-    B_ws_1st = B_gogn_1st[:,1]
-    B_Ms_1st = B_gogn_1st[:,5]
-    B_ws_2nd = B_gogn_2nd[:,1]
-    B_Ms_2nd = B_gogn_2nd[:,5]
+    ws = gogn[:,18]/sqrt(1.1)
+    Ms = gogn[:,7]*sqrt(1.1)
+    e_ws_1st = e_gogn_1st[:,18]/sqrt(1.1)
+    e_Ms_1st = e_gogn_1st[:,7]*sqrt(1.1)
+    e_ws_2nd = e_gogn_2nd[:,18]/sqrt(1.1)
+    e_Ms_2nd = e_gogn_2nd[:,7]*sqrt(1.1)
+    B_ws_1st = B_gogn_1st[:,1]/sqrt(1.1)
+    B_Ms_1st = B_gogn_1st[:,5]*sqrt(1.1)
+    B_ws_2nd = B_gogn_2nd[:,1]/sqrt(1.1)
+    B_Ms_2nd = B_gogn_2nd[:,5]*sqrt(1.1)
     B_ws = concatenate((B_ws_1st[::-1],B_ws_2nd))
     B_Ms = concatenate((B_Ms_1st[::-1],B_Ms_2nd))
     e_ws = concatenate((e_ws_1st,e_ws_2nd[::-1]))
@@ -38,16 +38,33 @@ plot_gogn(1,0,0)
 xlabel(r'$w$')
 ylabel(r'$M$')
 xlim(0.75,1.00)
-# curve = genfromtxt('zero-mode')
-# curve_w = curve[:,0]
-# curve_M = curve[:,1]
-# plot(curve_w,curve_M,'m')
-# no_int = genfromtxt('no-interaction-extremal')
-# no_int_w = no_int[:,0]
-# no_int_M = no_int[:,1]
-# plot(no_int_w,no_int_M,'k')
+curve = genfromtxt('zero-mode')
+curve_w = curve[:,0]
+curve_M = curve[:,1]
+plot(curve_w,curve_M,'m')
+no_int = genfromtxt('no-interaction-extremal')
+no_int_w = no_int[:,0]
+no_int_M = no_int[:,1]
+plot(no_int_w,no_int_M,'k')
 W = linspace(0.75,1.0,100)
 M = lambda w: 1/(2*w)
 plot(W,M(W),'k')
-legend()
-savefig('w-vs-M-all.png')
+legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),ncol=3,fancybox=True,shadow=True)
+
+# inset figure
+a = axes([0.2, 0.2, .4, .4])#, axisbg='y')
+plot_gogn(1,0,0)
+curve = genfromtxt('zero-mode')
+curve_w = curve[:,0]
+curve_M = curve[:,1]
+plot(curve_w,curve_M,'m')
+no_int = genfromtxt('no-interaction-extremal')
+no_int_w = no_int[:,0]
+no_int_M = no_int[:,1]
+plot(no_int_w,no_int_M,'k')
+W = linspace(0.75,1.0,100)
+M = lambda w: 1/(2*w)
+plot(W,M(W),'k')
+setp(a, xlim=(0.94,0.96),ylim=(0.4,0.7))#, xticks=[], yticks=[])
+
+savefig('w-vs-M-Q.png')
